cbc66c93268c8ff9166ed8084b21e9e4119a01c4
Fory Tech <fori@techsolutions.bg>
2025-08-24 10:36:28 +0000

    PQC: add missing modules + examples; fix test discovery/CI

--- sdk/src/foritech/crypto/__init__.py
+++ sdk/src/foritech/crypto/__init__.py
@@ -1,4 +1,3 @@
-
 from .pqc_kem import kem_generate, kem_encapsulate, kem_decapsulate, b64e, b64d
 from .hybrid_wrap import hybrid_wrap_dek, hybrid_unwrap_dek, hkdf
-
+__all__ = ["kem_generate","kem_encapsulate","kem_decapsulate","b64e","b64d","hybrid_wrap_dek","hybrid_unwrap_dek","hkdf"]
